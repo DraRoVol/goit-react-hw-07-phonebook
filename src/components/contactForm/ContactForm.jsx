18e4bf70a2b4811413f016953c47ada9d1ff8027
DraRoVol <107344569+DraRoVol@users.noreply.github.com>
2023-06-18 13:18:56 +0300

    Update ContactForm.jsx

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import cssModule from './ContactForm.module.css';
 import { useCreateContactMutation } from 'redux/contactsSlice';
 
